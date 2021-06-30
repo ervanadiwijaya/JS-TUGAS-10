d3e6eb1d2f306b23279a4253e04c8369e749c61e
jauhhari <ervanadiwijaya@gmail.com>
2021-06-30 15:37:54 +0700

    OK

---
+++ tugas10.js
@@ -0,0 +1,14 @@
+function bertingkat() {
+
+    var n = 10;
+    var bil = 0;
+
+    for (let i = 1; i < n; i++) {
+        bil += i;
+        // bil = ((i*i) + i)/2;
+        console.log(bil);
+    }
+    
+}
+
+bertingkat();
